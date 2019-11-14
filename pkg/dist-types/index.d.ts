c84c333455432ed65534f29c878b617f7c4efe70
Chip Allen <callen@ghx.com>
2019-11-14 11:02:13 -0800

    built

---
+++ pkg/dist-types/index.d.ts
@@ -0,0 +1,30 @@
+declare class PropertiesFile {
+    objs: {
+        [key: string]: any;
+    };
+    duplicateKeys: {
+        [file: string]: ({
+            [key: string]: number[];
+        });
+    };
+    constructor(...args: string[]);
+    makeKeys(line: string): string;
+    addFile(file: string): void;
+    of(...args: string[]): void;
+    get(key: string, defaultValue?: string): string | string[] | undefined;
+    getLast(key: string, defaultValue?: string): string | undefined;
+    getFirst(key: string, defaultValue?: string): string | undefined;
+    getInt(key: string, defaultIntValue?: number): number | undefined;
+    getFloat(key: string, defaultFloatValue?: number): number | undefined;
+    getBoolean(key: string, defaultBooleanValue?: boolean): boolean;
+    set(key: string, value: string): void;
+    interpolate(s: string): string;
+    getKeys(): string[];
+    getMatchingKeys(matchstr: string): string[];
+    getKeysForValue(value: string): string[];
+    flattenFirst(): this;
+    reset(): void;
+    hasDuplicateKeys(): boolean;
+}
+declare let of: (...args: any[]) => PropertiesFile;
+export { PropertiesFile, of };
